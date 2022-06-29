d7ed3a9820b0280252b0844c55c1197bad291439
DemonInTheCloset <demoninthecloset@gmail.com>
2022-06-29 15:41:33 +0200

    fix: Use default lualine config

--- init.lua
+++ init.lua
@@ -266,35 +266,7 @@ prequire("neogit").setup()
 -- [[ statusline ]] --
 local statusline = prequire "lualine"
 
-statusline.setup {
-	options = {
-		icons_enabled = true,
-		theme = "gruvbox",
-
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = {},
-		always_divide_middle = true,
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat" },
-		lualine_y = { "filetype" },
-		lualine_z = { "progress", "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
-}
+statusline.setup {}
 
 -- [[ augroups ]] --
 user_util.augroup("OpenFolds", {
