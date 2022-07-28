c84a31eccf693bda57dae4b82396d1b7bf23029b
DemonInTheCloset <demoninthecloset@gmail.com>
2022-07-28 21:50:50 +0200

    feat: Cleanup init.lua
    Rename:
    
    - vim.g -> g
    - vim.opt -> opt
    
    Use vim.opt instead of vim.o

--- init.lua
+++ init.lua
@@ -1,78 +1,81 @@
 -- Faster Lua
 require('impatient')
 
--- Enable Filetype Plugin
-vim.g.do_filetype_lua = 1 -- Use filetype.lua
--- vim.g.did_load_filetypes = 1 -- Disable filetype.vim
-vim.cmd "filetype plugin on"
-vim.g.mapleader = " "
-
+-- Constants
 local user_util = require "user/util"
 local prequire = user_util.prequire
+local CONFIG_PATH = user_util.CONFIG_PATH
+local g = vim.g
+local opt = vim.opt
+local fn = vim.fn
+
+-- Enable Filetype Plugin
+g.do_filetype_lua = 1 -- Use filetype.lua
+-- g.did_load_filetypes = 1 -- Disable filetype.vim
+vim.cmd "filetype plugin on"
+g.mapleader = " "
 
 -- Plugins
 prequire "user/packer"
 
 -- [[ nvim settings ]] --
-local CONFIG_PATH = user_util.CONFIG_PATH
-
 -- Providers
-vim.g.python3_host_prog = CONFIG_PATH .. "/.venv/bin/python"
+g.python3_host_prog = CONFIG_PATH .. "/.venv/bin/python"
 -- Disable unused
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
+g.loaded_node_provider = 0
+g.loaded_perl_provider = 0
 
 -- Appearance
-vim.opt.number = true -- Show line numbers
-vim.opt.relativenumber = true -- Show relative line numbers
-vim.opt.colorcolumn = "80" -- Color column 80
-vim.opt.cursorline = true -- Highlight selected line
-vim.opt.wrap = false -- Long lines don't wrap
-vim.opt.signcolumn = "number" -- Merge line numbers with coc messages
-vim.g.gruvbox_italic = 1 -- Allow italic font
+opt.number = true -- Show line numbers
+opt.relativenumber = true -- Show relative line numbers
+opt.colorcolumn = "80" -- Color column 80
+opt.cursorline = true -- Highlight selected line
+opt.wrap = false -- Long lines don't wrap
+opt.signcolumn = "number" -- Merge line numbers with coc messages
+g.gruvbox_italic = 1 -- Allow italic font
 
 -- In list mode show tabs and leading and trailing spaces
-vim.opt.listchars = { tab = "==>", trail = "·", lead = "·" }
+opt.listchars = { tab = "==>", trail = "·", lead = "·" }
 
 -- Colors
-if vim.fn.has "termguicolors" == 1 then
-	vim.o.termguicolors = true -- Allow Truecolor Support
+if fn.has "termguicolors" == 1 then
+	opt.termguicolors = true -- Allow Truecolor Support
 end
 vim.cmd "colorscheme gruvbox" -- Set colorscheme
 
 -- Folds
-vim.opt.foldmethod = "syntax"
+opt.foldmethod = "syntax"
 
 -- Statusbar
-vim.o.laststatus = 2 -- Statusbar is always visible
-vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
+opt.laststatus = 2 -- Statusbar is always visible
+g.qs_highlight_on_keys = { "f", "F", "t", "T" }
 
 -- Global Tab and indentation settings
-vim.o.smarttab = true -- Tabs are smarter
-vim.opt.autoindent = true -- Auto indent lines
-vim.opt.smartindent = true -- Indentation is smarter
-vim.opt.expandtab = true -- Change tabs to spaces
-vim.opt.tabstop = 4 -- A Tab is 4 spaces
-vim.opt.softtabstop = 0 -- No softtabs
-vim.opt.shiftwidth = 4 -- An indent is 4 spaces
+opt.smarttab = true -- Tabs are smarter
+opt.autoindent = true -- Auto indent lines
+opt.smartindent = true -- Indentation is smarter
+opt.expandtab = true -- Change tabs to spaces
+opt.tabstop = 4 -- A Tab is 4 spaces
+opt.softtabstop = 0 -- No softtabs
+opt.shiftwidth = 4 -- An indent is 4 spaces
 
 -- Search settings
-vim.o.path = ".,/usr/include,**"
-vim.o.wildmenu = true
-vim.o.hlsearch = true
-vim.o.incsearch = true
-vim.o.ignorecase = true -- Search ignores cases
-vim.o.smartcase = true -- Unless it has a capital letter
+opt.path = ".,/usr/include,**"
+opt.wildmenu = true
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true -- Search ignores cases
+opt.smartcase = true -- Unless it has a capital letter
 
 -- Options recommended by CoC
-vim.o.updatetime = 300 -- Update every 300 ms
-vim.o.timeoutlen = 200 -- Timeout after 200 ms
-vim.o.hidden = true -- TextEdit might fail otherwise
-vim.opt.shortmess:append "c" -- Don't pass messages to ins-completion-menu
-vim.o.cmdheight = 3 -- Give more space for messages
-vim.o.backup = false -- Some LSPs have problems with backups
-vim.o.writebackup = false
+opt.updatetime = 300 -- Update every 300 ms
+opt.timeoutlen = 200 -- Timeout after 200 ms
+opt.hidden = true -- TextEdit might fail otherwise
+opt.shortmess:append "c" -- Don't pass messages to ins-completion-menu
+opt.cmdheight = 3 -- Give more space for messages
+opt.backup = false -- Some LSPs have problems with backups
+opt.writebackup = false
 
 -- [[ Keymaps ]] --
 local keymaps = prequire "user/keymaps"
