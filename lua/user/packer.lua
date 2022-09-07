ceed5986fb55befb3b7dca4303ec41c7e2076dd2
DemonInTheCloset <demoninthecloset@gmail.com>
2022-09-07 16:15:58 +0200

    feat: Reformated and added Plugins

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -3,215 +3,266 @@ local fn = vim.fn
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 local packer_bootstrap = nil
 if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system {
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	}
+    packer_bootstrap = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
 end
 
 -- Plugins
 return require("packer").startup {
-	function(use)
-		use "wbthomason/packer.nvim"
+    function(use)
+        use "wbthomason/packer.nvim"
 
-		-- Time tracking
-		use "wakatime/vim-wakatime"
-		use "ActivityWatch/aw-watcher-vim"
+        -- Time tracking
+        use "wakatime/vim-wakatime"
+        use "ActivityWatch/aw-watcher-vim"
 
-		-- Vim Profiling
-		use { "tweekmonster/startuptime.vim", opt = true, cmd = { "StartupTime" } }
-		use "lewis6991/impatient.nvim"
+        -- Vim Profiling
+        use { "tweekmonster/startuptime.vim", opt = true, cmd = { "StartupTime" } }
+        use "lewis6991/impatient.nvim"
 
-		-- ColorScheme
-		use { "ellisonleao/gruvbox.nvim", requires = { { "rktjmp/lush.nvim" } } }
+        -- ColorScheme
+        use { "ellisonleao/gruvbox.nvim", requires = { { "rktjmp/lush.nvim" } } }
 
-		-- Colorizer
-		use {
-			"norcalli/nvim-colorizer.lua",
-			config = function()
-				require "user/plugins/colorizer"
-			end,
-		}
+        -- Colorizer
+        use {
+            "norcalli/nvim-colorizer.lua",
+            config = function()
+                require "user/plugins/colorizer"
+            end,
+        }
 
-		-- Jump hints
-		use {
-			"unblevable/quick-scope",
-			config = function()
-				vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
-			end,
-		}
+        -- Jump hints
+        use {
+            "unblevable/quick-scope",
+            config = function()
+                vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
+            end,
+        }
 
-		-- Show enclosing Scope at the Top
-		use {
-			"nvim-treesitter/nvim-treesitter-context",
-			config = function()
-				require("treesitter-context").setup {}
-			end,
-		}
+        -- Show enclosing Scope at the Top
+        use {
+            "nvim-treesitter/nvim-treesitter-context",
+            config = function()
+                require("treesitter-context").setup {}
+            end,
+        }
 
-		-- Align Text
-		use {
-			"junegunn/vim-easy-align",
-			opt = true,
-			cmd = { "<Plug>(EasyAlign)" },
-			config = function()
-				require("user/keymaps").easyalign_keys()
-			end,
-		}
+        -- Highlight Words and Lines under the cursor
+        use {
+            "yamatsum/nvim-cursorline",
+            config = function()
+                require("nvim-cursorline").setup {
+                    cursorline = {
+                        enable = true,
+                        timeout = 500,
+                        number = true,
+                    },
+                    cursorword = {
+                        enable = true,
+                        min_length = 3,
+                        hl = { underline = true },
+                    },
+                }
+            end,
+        }
 
-		-- Async commands
-		use {
-			"tpope/vim-dispatch",
-			opt = true,
-			cmd = { "Dispatch", "Make", "Focus", "Start" },
-			config = function()
-				require("user/keymaps").dispatch_keys()
-			end,
-		}
+        -- Align Text
+        use {
+            "junegunn/vim-easy-align",
+            opt = true,
+            cmd = { "<Plug>(EasyAlign)" },
+            config = function()
+                require("user/keymaps").easyalign_keys()
+            end,
+        }
 
-		-- Git integration
-		-- use "tpope/vim-fugitive"
-		use {
-			"TimUntersberger/neogit",
-			requires = { { "nvim-lua/plenary.nvim" } },
-			config = function()
-				require("neogit").setup {}
-				require("user/keymaps").neogit_keys()
-			end,
-		}
-		use {
-			"lewis6991/gitsigns.nvim",
-			config = function()
-				require("gitsigns").setup()
-			end,
-		}
+        -- Async commands
+        use {
+            "tpope/vim-dispatch",
+            opt = true,
+            cmd = { "Dispatch", "Make", "Focus", "Start" },
+            config = function()
+                require("user/keymaps").dispatch_keys()
+            end,
+        }
 
-		-- Language Server configuration
-		use {
-			"neovim/nvim-lspconfig",
-			config = function()
-				require "user/plugins/lspconfig"
-			end,
-		}
-		use "onsails/lspkind-nvim"
-		use "mfussenegger/nvim-jdtls" -- Java JDTLS support
-		use {
-			"simrat39/rust-tools.nvim",
-			requires = { { "nvim-lua/plenary.nvim" } },
-			config = function()
-				require "user/plugins/rust_tools"
-			end,
-		}
-		use {
-			"p00f/clangd_extensions.nvim",
-			config = function()
-				require "clangd_extensions"
-			end,
-		}
+        -- Run Tests
+        use {
+            "nvim-neotest/neotest",
+            requires = {
+                "nvim-lua/plenary.nvim",
+                "nvim-treesitter/nvim-treesitter",
+                "antoinemadec/FixCursorHold.nvim",
+                "rouge8/neotest-rust",
+            },
+            config = function()
+                require("neotest").setup {
+                    adapters = {
+                        require "neotest-rust",
+                    },
+                }
+            end,
+        }
+        use {
+            "CRAG666/code_runner.nvim",
+            requires = "nvim-lua/plenary.nvim",
+            config = function()
+                require("code_runner").setup {
+                    mode = "tab",
+                    focus = false,
+                    -- put here the commands by filetype
+                    filetype = {
+                        rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
+                    },
+                }
+            end,
+        }
 
-		-- Debugging
-		use "mfussenegger/nvim-dap"
+        -- Git integration
+        -- use "tpope/vim-fugitive"
+        use {
+            "TimUntersberger/neogit",
+            requires = { { "nvim-lua/plenary.nvim" } },
+            config = function()
+                require("neogit").setup {}
+                require("user/keymaps").neogit_keys()
+            end,
+        }
+        use {
+            "lewis6991/gitsigns.nvim",
+            config = function()
+                require("gitsigns").setup()
+            end,
+        }
 
-		-- Completion Support
-		use "hrsh7th/cmp-nvim-lsp" -- Language server hints
-		use "hrsh7th/cmp-buffer" -- Words from the buffer
-		use "hrsh7th/cmp-path" -- Path like strings (ie. /usr/bin/nvim)
-		use "hrsh7th/cmp-cmdline" -- Commands completion
-		use "saadparwaiz1/cmp_luasnip" -- Snippet support
-		use {
-			"hrsh7th/nvim-cmp",
-			config = function()
-				require "user/plugins/cmp"
-			end,
-		}
+        -- Language Server configuration
+        use {
+            "neovim/nvim-lspconfig",
+            config = function()
+                require "user/plugins/lspconfig"
+            end,
+        }
+        use "onsails/lspkind-nvim"
+        use "mfussenegger/nvim-jdtls" -- Java JDTLS support
+        use {
+            "simrat39/rust-tools.nvim",
+            requires = { { "nvim-lua/plenary.nvim" } },
+            config = function()
+                require "user/plugins/rust_tools"
+            end,
+        }
+        use {
+            "p00f/clangd_extensions.nvim",
+            config = function()
+                require "clangd_extensions"
+            end,
+        }
 
-		-- Formatting support
-		use {
-			"jose-elias-alvarez/null-ls.nvim",
-			config = function()
-				require "user/plugins/null_ls"
-			end,
-		}
+        -- Debugging
+        use "mfussenegger/nvim-dap"
 
-		-- Snippet support
-		use {
-			"L3MON4D3/LuaSnip",
-			config = function()
-				local CONFIG_PATH = require("user/util").CONFIG_PATH
+        -- Completion Support
+        use "hrsh7th/cmp-nvim-lsp" -- Language server hints
+        use "hrsh7th/cmp-buffer" -- Words from the buffer
+        use "hrsh7th/cmp-path" -- Path like strings (ie. /usr/bin/nvim)
+        use "hrsh7th/cmp-cmdline" -- Commands completion
+        use "saadparwaiz1/cmp_luasnip" -- Snippet support
+        use {
+            "hrsh7th/nvim-cmp",
+            config = function()
+                require "user/plugins/cmp"
+            end,
+        }
 
-				require("luasnip").config.setup { update_events = "TextChanged,TextChangedI" }
-				require("luasnip.loaders.from_lua").load { paths = CONFIG_PATH .. "/snippets" }
+        -- Formatting support
+        use {
+            "jose-elias-alvarez/null-ls.nvim",
+            config = function()
+                require "user/plugins/null_ls"
+            end,
+        }
 
-				require("user/keymaps").luasnip_keys(require "luasnip")
-			end,
-		}
+        -- Snippet support
+        use {
+            "L3MON4D3/LuaSnip",
+            config = function()
+                local CONFIG_PATH = require("user/util").CONFIG_PATH
 
-		-- StatusLine
-		use {
-			"nvim-lualine/lualine.nvim",
-			requires = { "kyazdani42/nvim-web-devicons", opt = true },
-			config = function()
-				require("lualine").setup {}
-			end,
-		}
+                require("luasnip").config.setup { update_events = "TextChanged,TextChangedI" }
+                require("luasnip.loaders.from_lua").load { paths = CONFIG_PATH .. "/snippets" }
 
-		-- Filetype Highlighting
-		use {
-			"nvim-treesitter/nvim-treesitter",
-			run = ":TSUpdate",
-			config = function()
-				require "user/plugins/treesitter"
-			end,
-		}
-		use { "baskerville/vim-sxhkdrc", ft = "sxhkdrc" }
-		use { "chunkhang/vim-mbsync", ft = "mbsyncrc" }
-		-- use "https://gitlab.com/DemonInTheCloset/porth-vim.git"
+                require("user/keymaps").luasnip_keys(require "luasnip")
+            end,
+        }
 
-		use {
-			"iamcco/markdown-preview.nvim",
-			run = function()
-				vim.fn["mkdp#util#install"]()
-			end,
-		}
+        -- StatusLine
+        use {
+            "nvim-lualine/lualine.nvim",
+            requires = { "kyazdani42/nvim-web-devicons", opt = true },
+            config = function()
+                require("lualine").setup {}
+            end,
+        }
 
-		use "nvim-lua/plenary.nvim"
-		-- Fuzzy find
-		use {
-			"nvim-telescope/telescope.nvim",
-			requires = {
-				{ "nvim-lua/plenary.nvim" },
-				{ "nvim-telescope/telescope-ui-select.nvim" },
-				{ "nvim-telescope/telescope-fzy-native.nvim" },
-			},
-			config = function()
-				require "user/plugins/telescope"
-			end,
-		}
+        -- Filetype Highlighting
+        use {
+            "nvim-treesitter/nvim-treesitter",
+            run = ":TSUpdate",
+            config = function()
+                require "user/plugins/treesitter"
+            end,
+        }
+        use { "baskerville/vim-sxhkdrc", ft = "sxhkdrc" }
+        use { "chunkhang/vim-mbsync", ft = "mbsyncrc" }
+        -- use "https://gitlab.com/DemonInTheCloset/porth-vim.git"
 
-		use {
-			"hood/popui.nvim",
-			requires = { { "RishabhRD/popfix" } },
-			config = function()
-				vim.ui.input = require "popui.input-overrider"
-			end,
-		}
+        use {
+            "iamcco/markdown-preview.nvim",
+            run = function()
+                vim.fn["mkdp#util#install"]()
+            end,
+        }
 
-		-- Do bootstrap
-		if packer_bootstrap then
-			require("packer").sync()
-		end
-	end,
-	config = {
-		display = {
-			open_fn = require("packer.util").float,
-		},
-		profile = {
-			enable = true,
-			threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
-		},
-	},
+        use "nvim-lua/plenary.nvim"
+        -- Fuzzy find
+        use {
+            "nvim-telescope/telescope.nvim",
+            requires = {
+                { "nvim-lua/plenary.nvim" },
+                { "nvim-telescope/telescope-ui-select.nvim" },
+                { "nvim-telescope/telescope-fzy-native.nvim" },
+            },
+            config = function()
+                require "user/plugins/telescope"
+            end,
+        }
+
+        use {
+            "hood/popui.nvim",
+            requires = { { "RishabhRD/popfix" } },
+            config = function()
+                vim.ui.input = require "popui.input-overrider"
+            end,
+        }
+
+        -- Do bootstrap
+        if packer_bootstrap then
+            require("packer").sync()
+        end
+    end,
+    config = {
+        display = {
+            open_fn = require("packer.util").float,
+        },
+        profile = {
+            enable = true,
+            threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+        },
+    },
 }
