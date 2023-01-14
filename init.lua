c90fe6450317169b103194e6e4d87f6dded0ad8a
DemonInTheCloset <demoninthecloset@gmail.com>
2023-01-14 14:57:29 +0100

    feat: Transparent backgroudns

--- init.lua
+++ init.lua
@@ -46,6 +46,8 @@ if fn.has "termguicolors" == 1 then
 	opt.termguicolors = true -- Allow Truecolor Support
 end
 vim.cmd.colorscheme "gruvbox" -- Set colorscheme
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 -- Folds
 opt.foldmethod = "syntax"
