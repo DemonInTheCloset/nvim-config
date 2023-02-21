d5661f39e3c1289cb7d5d8be4fee5dd03b00a495
DemonInTheCloset <demoninthecloset@gmail.com>
2023-02-21 18:27:19 +0100

    fix: Defaulted mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,18 +1,16 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        name = "mason",
-        cmd = { "Mason", "MasonLog", "MasonInstall", "MasonUninstall", "MasonUninstallAll" },
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		name = "mason",
+		cmd = { "Mason", "MasonLog", "MasonInstall", "MasonUninstall", "MasonUninstallAll" },
+		config = true,
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        name = "mason-lspconfig",
-        dependencies = { "mason", "lspconfig" },
-        opts = { automatic_installation = true },
-    },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		name = "mason-lspconfig",
+		dependencies = { "mason", "lspconfig" },
+		opts = { automatic_installation = true },
+	},
 }
