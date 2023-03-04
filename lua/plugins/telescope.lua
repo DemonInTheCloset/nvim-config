60a613daaba037cbd425c8b5c76f5c51c9f80e9b
DemonInTheCloset <demoninthecloset@gmail.com>
2023-03-04 15:06:30 +0100

    fix(telescope): Failing on first install

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,15 +8,17 @@ return {
 		},
 		cmd = 'Telescope',
 		keys = function()
-			local telescope_builtin = require 'telescope.builtin'
-			return {
-				{ '<leader>ff', telescope_builtin.find_files, desc = '[F]ind [F]iles' },
-				{ '<leader>fg', telescope_builtin.live_grep, desc = '[F]ind [G]rep' },
-				{ '<leader>fh', telescope_builtin.help_tags, desc = '[F]ind [H]elp' },
-				{ '<leader>fb', telescope_builtin.buffers, desc = '[F]ind [B]uffer' },
-				{ '<leader>fd', telescope_builtin.diagnostics, desc = '[F]ind [D]iagnostics' },
-				{ '<leader>fq', telescope_builtin.quickfix, desc = '[F]ind [Q]uickfix' },
-			}
+			local ok, telescope_builtin = pcall(require, 'telescope.builtin')
+			if ok then
+				return {
+					{ '<leader>ff', telescope_builtin.find_files, desc = '[F]ind [F]iles' },
+					{ '<leader>fg', telescope_builtin.live_grep, desc = '[F]ind [G]rep' },
+					{ '<leader>fh', telescope_builtin.help_tags, desc = '[F]ind [H]elp' },
+					{ '<leader>fb', telescope_builtin.buffers, desc = '[F]ind [B]uffer' },
+					{ '<leader>fd', telescope_builtin.diagnostics, desc = '[F]ind [D]iagnostics' },
+					{ '<leader>fq', telescope_builtin.quickfix, desc = '[F]ind [Q]uickfix' },
+				}
+			end
 		end,
 		opts = function()
 			-- Settings
