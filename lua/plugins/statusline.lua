6e1e11b4d8bb67aac7fd2227d052017932c74f18
DemonInTheCloset <demoninthecloset@gmail.com>
2023-03-05 08:01:37 +0100

    fix(nvim-wed-devicons): Bad repo name

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,7 +2,7 @@ return {
 	{
 		'nvim-lualine/lualine.nvim',
 		lazy = false,
-		dependencies = { 'kyazdani42/nvim-web-devicons' },
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		name = 'lualine',
 		opts = { options = { theme = 'gruvbox-baby' } },
 	},
