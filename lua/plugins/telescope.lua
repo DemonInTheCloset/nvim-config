dd46bb23dc3ad337f5e45965dd9da651aa54da6a
DemonInTheCloset <demoninthecloset@gmail.com>
2023-03-05 08:12:36 +0100

    feat(telescope): Add git/mercurial aware find_files/live_grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,8 +9,43 @@ return {
 		cmd = 'Telescope',
 		keys = function()
 			local ok, telescope_builtin = pcall(require, 'telescope.builtin')
+
+			local function is_repo(cmd)
+				vim.fn.system(cmd)
+				return vim.v.shell_error == 0
+			end
+
+			local function find_root(name)
+				local dot_repo_path = vim.fn.finddir(name, '.;')
+				return vim.fn.fnamemodify(dot_repo_path, ':h')
+			end
+
+			local function git_live_grep()
+				local opts = {}
+				if is_repo 'git rev-parse --is-inside-work-tree' then
+					opts.cwd = find_root '.git'
+				elseif is_repo 'hg root' then
+					opts.cwd = find_root '.hg'
+				end
+
+				telescope_builtin.live_grep(opts)
+			end
+
+			local function git_find_files()
+				local opts = {}
+				if is_repo 'git rev-parse --is-inside-work-tree' then
+					opts.cwd = find_root '.git'
+				elseif is_repo 'hg root' then
+					opts.cwd = find_root '.hg'
+				end
+
+				telescope_builtin.find_files(opts)
+			end
+
 			if ok then
 				return {
+					{ '<leader>gf', git_find_files, desc = '[G]it [F]iles' },
+					{ '<leader>gg', git_live_grep, desc = '[G]it [G]rep' },
 					{ '<leader>ff', telescope_builtin.find_files, desc = '[F]ind [F]iles' },
 					{ '<leader>fg', telescope_builtin.live_grep, desc = '[F]ind [G]rep' },
 					{ '<leader>fh', telescope_builtin.help_tags, desc = '[F]ind [H]elp' },
