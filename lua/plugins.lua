8bf01be23b223a09e8ce10f1eeca52a7fdf2781d
DemonInTheCloset <demoninthecloset@gmail.com>
2023-03-05 18:11:22 +0100

    feat: More keybindings for FTerm

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,10 +8,16 @@ return {
 		'numToStr/FTerm.nvim',
 		name = 'FTerm',
 		keys = function()
+			local fterm = require 'FTerm'
+			local function oneshot(opts)
+				return function() fterm.scratch(opts) end
+			end
 			return {
-				{ '<leader>to', require('FTerm').open, desc = '[T]erminal [O]pen' },
-				{ '<leader>tc', require('FTerm').close, desc = '[T]erminal [C]lose' },
-				{ '<leader>te', require('FTerm').exit, desc = '[T]erminal [E]xit' },
+				{ '<leader>tt', fterm.toggle, desc = '[T]erminal [T]oggle' },
+				{ '<leader>to', fterm.open, desc = '[T]erminal [O]pen' },
+				{ '<leader>tc', fterm.close, desc = '[T]erminal [C]lose' },
+				{ '<leader>te', fterm.exit, desc = '[T]erminal [E]xit' },
+				{ '<leader>tg', oneshot { cmd = 'lazygit' }, desc = '[T]erminal lazy[G]it' },
 			}
 		end,
 		opts = {},
