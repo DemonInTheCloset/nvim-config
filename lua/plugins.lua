d85eafab057cd9e2fde16f7e722d325126796a41
DemonInTheCloset <demoninthecloset@gmail.com>
2023-03-13 22:46:57 -0300

    fix(plugins): Keymaps loading plugin before it exists (on a fresh install)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,17 +13,20 @@ return {
 		'numToStr/FTerm.nvim',
 		name = 'FTerm',
 		keys = function()
-			local fterm = require 'FTerm'
-			local function oneshot(opts)
-				return function() fterm.scratch(opts) end
+			local ok, fterm = pcall(require, 'FTerm')
+			if ok then
+				local function oneshot(opts)
+					return function() fterm.scratch(opts) end
+				end
+				-- Keymaps
+				return {
+					{ '<leader>tt', fterm.toggle, desc = '[T]erminal [T]oggle' },
+					{ '<leader>to', fterm.open, desc = '[T]erminal [O]pen' },
+					{ '<leader>tc', fterm.close, desc = '[T]erminal [C]lose' },
+					{ '<leader>te', fterm.exit, desc = '[T]erminal [E]xit' },
+					{ '<leader>tg', oneshot { cmd = 'lazygit' }, desc = '[T]erminal lazy[G]it' },
+				}
 			end
-			return {
-				{ '<leader>tt', fterm.toggle, desc = '[T]erminal [T]oggle' },
-				{ '<leader>to', fterm.open, desc = '[T]erminal [O]pen' },
-				{ '<leader>tc', fterm.close, desc = '[T]erminal [C]lose' },
-				{ '<leader>te', fterm.exit, desc = '[T]erminal [E]xit' },
-				{ '<leader>tg', oneshot { cmd = 'lazygit' }, desc = '[T]erminal lazy[G]it' },
-			}
 		end,
 		opts = {},
 	},
