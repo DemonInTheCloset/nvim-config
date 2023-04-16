aa9f04df6e0fc5b5490442b82a0e05e2832dc7f1
DemonInTheCloset <demoninthecloset@gmail.com>
2023-04-16 20:06:11 +0200

    feat: Customized statusline (added lsp-progress info)

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,8 +2,35 @@ return {
 	{
 		'nvim-lualine/lualine.nvim',
 		lazy = false,
-		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		dependencies = { 'nvim-tree/nvim-web-devicons', 'arkav/lualine-lsp-progress' },
 		name = 'lualine',
-		opts = { options = { theme = 'gruvbox-baby' } },
+		opts = {
+			options = { theme = 'gruvbox' },
+			extensions = { 'quickfix', 'lazy', 'man' },
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'filename', 'branch' },
+				lualine_c = {
+					{
+						'lsp_progress',
+						separators = {
+							component = ' ',
+							progress = ' | ',
+							percentage = { pre = '', post = '%% ' },
+							title = { pre = '', post = ': ' },
+							lsp_client_name = { pre = '[', post = ']' },
+							spinner = { pre = '', post = '' },
+							message = { pre = '(', post = ')', commenced = 'In Progress', completed = 'Completed' },
+						},
+						display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
+						timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
+						spinner_symbols = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ' },
+					},
+				},
+				lualine_x = {},
+				lualine_y = { 'encoding', 'fileformat', 'filetype' },
+				lualine_z = { 'location' },
+			},
+		},
 	},
 }
