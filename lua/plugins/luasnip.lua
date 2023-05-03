2d822b6ac7e83f7d2b36e42d13878eeb2a4f658a
DemonInTheCloset <demoninthecloset@gmail.com>
2023-05-03 22:36:39 +0200

    fix: Formatting

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,8 +2,6 @@ return {
 	{
 		'L3MON4D3/LuaSnip',
 		name = 'luasnip',
-		config = function()
-			require('luasnip').config.setup { update_events = 'TextChanged,TextChangedI' }
-		end,
+		config = function() require('luasnip').config.setup { update_events = 'TextChanged,TextChangedI' } end,
 	},
 }
